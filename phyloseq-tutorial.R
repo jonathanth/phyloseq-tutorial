177813d6e8a77ecc5e96c6b10d416806b660721c
jonathanth <jonathan.thorsen@dbac.dk>
2022-05-25 12:47:19 +0200

    small additions

--- phyloseq-tutorial.R
+++ phyloseq-tutorial.R
@@ -2,10 +2,11 @@
 library(tidyverse)
 library(phyloseq)
 
-# Some nice tutorials available on https://joey711.github.io/phyloseq/
+# Some nice tutorials available on the website: https://joey711.github.io/phyloseq/
 # Nice overview in phyloseq paper: https://journals.plos.org/plosone/article/figure?id=10.1371/journal.pone.0061217.g003
 
-# Try with some COPSAC data
+# Do the examples below with some real COPSAC 16S data.
+# Does not work outside COPSAC, but can be substituted with any other phyloseq object for these examples.
 load("/Volumes/Research/Microbiology/Shared/DADA2 Golden Russel Jul 2020/dada2_gtdb_2020_07_13.RData")
 
 # 5 "slots" of connected data. It's super handy that phyloseq takes care of all the organization when subsetting, transforming, pruning etc.
@@ -24,16 +25,19 @@ otu_table(phy) %>% as.matrix %>% class  # does not hard convert
 otu_table(phy) %>% as("matrix") %>% class  # does hard convert
 otu_table(phy) %>% as.data.frame %>% class  # does hard convert, takes long time
 otu_table(phy) %>% as("matrix") %>% as.data.frame %>% class  # does hard convert, goes faster!
+# Get sample sums/totals
 sample_sums(phy)
+# Get totals for each taxon
 taxa_sums(phy)
 
 ####
-# tax_table is a special char. matrix that contains taxonomy information on features/ASVs. Can also put other feature metadata in there.
+# tax_table is a special char. matrix that contains taxonomy information on features/ASVs. Can also put other feature metadata in there, but preferably to the right of the taxonomy as the hierarchy is required for tax_glom, see below
 tax_table(phy)[1:10,]
 tax_table(phy) %>% as.matrix %>% class  # does not hard convert
 tax_table(phy) %>% as("matrix") %>% class  # does hard convert
 # tax_table(phy) %>% as.data.frame %>% class  # does hard convert, takes long time
 tax_table(phy) %>% as("matrix") %>% as.data.frame %>% class  # does hard convert, goes faster!
+# Inspect the names of each rank
 rank_names(phy)
 
 ####
@@ -46,6 +50,7 @@ sample_data(phy) %>% as("data.frame") %>% class # does hard convert
 # another handy function to inspect/extract sample metadata.
 get_variable(phy) %>% head
 get_variable(phy, c("SampleID", "ABCNO")) %>% head
+# Inspect variable names
 sample_variables(phy)
 
 ####
@@ -53,7 +58,7 @@ sample_variables(phy)
 phy_tree(phy)
 
 ###
-# refseq are the reference sequences
+# refseq are the reference sequences. Can be extracted and saved, blasted, compared to other studies, or whatever's needed
 refseq(phy)
 refseq(phy) %>% as.character %>% head
 # 
@@ -67,7 +72,9 @@ phy_small <- subset_samples(phy, Tray == "Tray01")
 # Prune on tax abundances, good idea to always do after subsetting
 phy_small <- phy_small %>% prune_taxa(taxa_sums(.) > 0, .)
 
-# If you want eg. only 
+# If you want eg. only 1 month Tracheal samples
+phy_t1m <- subset_samples(phy, Sampletype == "Trachea" & Time == "1m")
+# remember to prune taxa after!
 
 # Subset on taxonomy
 phy_small %>% tax_table %>% head
@@ -82,6 +89,7 @@ phy_important <- prune_samples(important_samples, phy)
 phy_relabu <- phy_small %>% transform_sample_counts(function(x) x/sum(x))
 sample_sums(phy_relabu)
 
+# Extract counts and convert to z-scaled log relative abundances with pseudocount. Useful for downstream analysis, eg PLS, RF.
 zrelabucounts <- phy_relabu %>% 
   otu_table %>% 
   as("matrix") %>% 
@@ -190,11 +198,13 @@ make_mradat <- function(phy){
 mradat <- phy_small %>% make_mradat
 head(mradat)
 
+# Choose taxa above the line in the plot below
 chosen_taxa <- mradat %>% 
   filter(prevalence >= 0.25*(1e-4/mra)^.5) %>% 
   pull(tax) %>% 
   as.character
 
+## MRA vs. prevalence of entire dataset, useful for making cutoffs for choosing common taxa for DA testing or machine learning
 ## Needs fullscreen
 ggplot(mradat, aes(mra, prevalence)) +
   geom_point(aes(color = Phylum)) +
@@ -203,5 +213,6 @@ ggplot(mradat, aes(mra, prevalence)) +
   stat_function(fun = function(x) 0.25*(1e-4/x)^.5, linetype = "dashed", inherit.aes = FALSE) +
   coord_cartesian(ylim = c(0, 1))
 
+# Can also do mradats on agglomerated versions
 mradat_f <- phy_small_family %>% make_mradat
 mradat_f %>% arrange(desc(prevalence)) %>% head
